18c65d13f127583e44685345f6f8873b623c81c3
Michael Ilg <michael@ilg.ac>
2017-01-02 21:25:40 -0800

    Setup socket.io for react

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import socket from './socketio.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+  }
+
+  componentDidMount() {
+    socket.on('news', function (data) {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
   }
 
   render() {

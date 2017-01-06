489efe79cc899d2c2c2e4f6a58c24211725f7ea5
Michael Ilg <michael@ilg.ac>
2017-01-06 12:22:11 -0800

    Integrate passport with JWT on server

--- server/server.js
+++ server/server.js
@@ -5,15 +5,17 @@ var io = require('socket.io')(server);
 var path = require('path');
 var morgan = require('morgan');
 var colors = require('colors/safe');
+// var passport = require('passport');
+var bodyParser = require('body-parser');
+
+var auth = require("./lib/auth.js")();
 var apiRouter = require('./routes/apiRouter');
 require('./io.js')(io);
-var passport = require('passport');
-var bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.use(passport.initialize());
+app.use(auth.initialize());
 
 app.use(express.static('client'));
 
@@ -31,13 +33,33 @@ app.get('*', function (req, res) {
 //   console.error(err.stack);
 //   res.status(500).send('Oops! Something broke on our end. Please refresh');
 // });
-require('./lib/auth.js')(passport);
 
-app.get('/api/autTest', passport.authenticate('jwt', { session: false}),
-  function(req, res) {
-      res.send('Hey there authenticated user');
-  }
-);
+app.get("/authTest", auth.authenticate(), function(req, res) {
+    res.send('Hey there authenticated user');
+});
+
+app.post("/token", function(req, res) {
+    if (req.body.email && req.body.password) {
+        var email = req.body.email;
+        var password = req.body.password;
+        var user = users.find(function(u) {
+            return u.email === email && u.password === password;
+        });
+        if (user) {
+            var payload = {
+                id: user.id
+            };
+            var token = jwt.encode(payload, cfg.jwtSecret);
+            res.json({
+                token: token
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+});
 
 server.listen(3000, function () {
   console.log(colors.green('\nReactivity app listening on port 3000!'));

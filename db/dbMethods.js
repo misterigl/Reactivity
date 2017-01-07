9fc2cc679c0540023861889252252e46e24cea81
Nate Graf <njgraf512@gmail.com>
2017-01-06 16:48:52 -0800

    Create activities nearby API

--- db/dbMethods.js
+++ db/dbMethods.js
@@ -4,20 +4,35 @@ var st = knex.postgis;
 var Activity = require('./models/Activity.js');
 
 
-exports.activitiesNearby = function() {
-  Activity
+exports.activitiesNearby = function(lat, long, n) {
+  var lat = lat.toString();
+  var long = long.toString();
+  return Activity
     .query()
     // Doing JoinEagerAlgorithm instead of WhereInEagerAlgorithm
     .eagerAlgorithm(Activity.JoinEagerAlgorithm)
-    .eager('location')
-    .modifyEager('location', function(builder) {
-      builder.orderBy('geom', '<->', st.geomFromText('Point(37.784118 122.408918)', 4326));
-      // builder.limit(10);
-    })
-    .limit(10)
+    .eager('[location, sport]')
+    .orderBy('location.geom', '<->', st.geomFromText('Point(' + lat + ' ' + long + ')', 4326))
+    .limit(n)
+    .debug()
     .then(function(results) {
-      console.log('results: ', results);
+      return results;
     });
 };
 
-exports.activitiesNearby();
+// exports.activitiesNearby = function() {
+//   Activity
+//     .query()
+//     // Doing JoinEagerAlgorithm instead of WhereInEagerAlgorithm
+//     .eagerAlgorithm(Activity.JoinEagerAlgorithm)
+//     .eager('location')
+//     .modifyEager('location', function(builder) {
+//       builder.orderBy('geom', '<->', st.geomFromText('Point(37.784118 122.408918)', 4326));
+//       builder.limit(10);
+//     })
+//     // .limit(10)
+//     .then(function(results) {
+//       console.log('results: ', results);
+//     });
+// };
+
--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,9 +1,25 @@
 var apiRouter = require('express').Router();
+var dbMethods = require('../../db/dbMethods');
 
 apiRouter.route('/activities')
   .get(function(req, res) {
     res.send('Request for activities');
   });
+
+apiRouter.get('/activities/nearby/:lat/:long/:n?', function(req, res) {
+  var lat = req.params.lat;
+  var long = req.params.long;
+  var n = req.params.n || 10;
+
+  dbMethods.activitiesNearby(lat, long, n)
+    .then(function(results) {
+      res.json(results);
+    })
+    .catch(function(err) {
+      console.error('find nearby activities error:', err);
+      res.status(500).send(err);
+    });
+});
 
 apiRouter.route('/test')
   .get(function(req, res) {

ffbe4ac05ede71e140aaf8686e20fc1029c4fe77
Nate Graf <njgraf512@gmail.com>
2017-01-16 22:26:17 -0800

    Set up cognito get token for amazon client on backend

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -7,6 +7,7 @@ var Promise = require('bluebird');
 var bcrypt = require('bcryptjs');
 bcrypt.compare = Promise.promisify(bcrypt.compare);
 var dbMethods = require('../../db/dbMethods.js');
+var getCognitoToken = require('../lib/awsCognitoAuth.js').getCognitoToken;
 
 authRouter.get('/test', function(req, res) {
   User.query()
@@ -15,6 +16,17 @@ authRouter.get('/test', function(req, res) {
     })
     .catch(function(err) {
       console.error(err);
+      res.json(err);
+    });
+});
+
+authRouter.get('/test2', function(req, res) {
+  getCognitoToken(24)
+    .then(function(result) {
+      res.json(result);
+    })
+    .catch(function(err) {
+      console.error('error: ', err);
       res.json(err);
     });
 });
@@ -43,11 +55,18 @@ authRouter.post('/login', function(req, res) {
         throw 'Invalid password';
       } else {
         var payload = { id: user.id, exp: moment().add(30, 'days').unix() };
-        var token = jwt.encode(payload, auth.cfg.jwtSecret);
-        res.json({ token: token, userId: user.id, username: user.username });
+        return Promise.all([
+          getCognitoToken(user.id),
+          jwt.encode(payload, auth.cfg.jwtSecret),
+          user
+        ]);
       }
     })
+    .spread(function(cognitoObj, token, user) {
+      res.json({ token: token, userId: user.id, username: user.username, amazon: cognitoObj });
+    })
     .catch(function(authError) {
+      console.error(authError);
       res.status(401).send(authError);
     })
     .error(function(err) {
